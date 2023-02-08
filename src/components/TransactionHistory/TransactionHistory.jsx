cb49e6381edd19dc4263576c5370733d628b23a8
Iryna Khamidova <waterholein@gmail.com>
2023-02-08 20:58:23 +0200

    Add components

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export function TransactionHistory({ items }) {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.tableHead}>
+        <tr className={css.tableLine}>
+          <th className={css.tableColumn}>Type</th>
+          <th className={css.tableColumn}>Amount</th>
+          <th className={css.tableColumn}>Currency</th>
+        </tr>
+      </thead>
+      <tbody className={css.tableBody}>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={css.tableLine} key={id}>
+            <td className={css.tableColumn}>{type}</td>
+            <td className={css.tableColumn}>{amount}</td>
+            <td className={css.tableColumn}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
